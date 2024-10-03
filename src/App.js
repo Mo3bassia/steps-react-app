1a5acec4c21ffdbbec7ba18be946b511093ba02b
Mohamed Abassia <mo3bassia@gmail.com>
2024-10-03 09:21:29 +0300

    add react state logic

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
+import { useState } from "react";
+
 const messages = [
-  "Learn React ⚛️",
+  "Learn React Js ⚛️",
   "Apply for jobs 💼",
   "Invest your new income 🤑",
 ];
 
 export default function App() {
-  let step = 2;
+  // let step = 2;
+  let [step, setStep] = useState(1);
+
+  function handleNext() {
+    step < messages.length && setStep(step + 1);
+  }
+
+  function handlePrevious() {
+    step > 1 && setStep(step - 1);
+  }
+
   return (
     <div className="steps">
       <Numbers step={step} />
       <Paragraph text={`Step ${step}: ${messages[step - 1]}`} />
       <div className="buttons">
-        <Button text="Previous" />
-        <Button text="Next" />
+        <button
+          text="Previous"
+          style={{ backgroundColor: "#7950F2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
+          Previous
+        </button>
+        <button
+          text="Next"
+          style={{ backgroundColor: "#7950F2", color: "#fff" }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
@@ -21,21 +45,21 @@ export default function App() {
 function Numbers({ step }) {
   return (
     <div className="numbers">
-      <div className={step >= 1 ? "active" : ""}>1</div>
+      {messages.map((message, index) => (
+        <div
+          className={step >= `${index + 1}` ? "active" : ""}
+          key={`div-${index + 1}`}
+        >
+          {index + 1}
+        </div>
+      ))}
+      {/* <div className={step >= 1 ? "active" : ""}>1</div>
       <div className={step >= 2 ? "active" : ""}>2</div>
-      <div className={step >= 3 ? "active" : ""}>3</div>
+      <div className={step >= 3 ? "active" : ""}>3</div> */}
     </div>
   );
 }
 
 function Paragraph({ text }) {
   return <p className="message">{text}</p>;
-}
-
-function Button({ text }) {
-  return (
-    <button style={{ backgroundColor: "#7950F2", color: "#fff" }}>
-      {text}
-    </button>
-  );
 }
